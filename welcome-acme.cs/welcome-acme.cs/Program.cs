c2ad47969aa7393f7be77c7b3456b3b7f7b16f5a
Robert <rbmanez@gmail.com>
2019-09-09 13:49:47 -0600

    create simple welcome app

---
+++ welcome-acme.cs/welcome-acme.cs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace welcome_acme.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
