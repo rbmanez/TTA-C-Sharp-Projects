1bbe32a5c041391d24e876581b6f5cfee90e8ca4
Robert <rbmanez@gmail.com>
2019-09-09 15:16:47 -0600

    add data type exercise app

---
+++ data-type-ex.cs/data-type-ex.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_type_ex.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string course_num = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string page_num = Console.ReadLine();
+            Console.WriteLine("Do you need help? Please answer \"true\" or \"false\"");
+            string help = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positive_exp = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string study_hours = Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
