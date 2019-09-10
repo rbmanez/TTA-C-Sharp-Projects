2eebea7595b22f95b4d099f3ed06a0271151b3dd
Robert <rbmanez@gmail.com>
2019-09-10 12:30:24 -0600

    create boolean logic exercise

---
+++ boolean-exercise.cs/boolean-exercise.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace boolean_exercise.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Applicants must be over 15 years old.
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            bool age_over15 = Convert.ToInt32(age) > 15;
+
+            //Applicants must not have any DUI’s.
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+            bool zero_dui = Convert.ToBoolean(dui) == false;
+
+            //Applicants must not have more than 3 speeding tickets.
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tickets = Console.ReadLine();
+            bool few_tickets = Convert.ToInt32(tickets) <= 3;
+
+            //Print the result
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age_over15 && zero_dui && few_tickets);
+            Console.ReadLine();
+        }
+    }
+}
