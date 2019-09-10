489ca743a2c588e8599997efd8735411158b6359
Robert <rbmanez@gmail.com>
2019-09-10 14:08:16 -0600

    add comments

--- data-type-ex.cs/data-type-ex.cs/Program.cs
+++ data-type-ex.cs/data-type-ex.cs/Program.cs
@@ -4,22 +4,34 @@ namespace data_type_ex.cs
 {
     class Program
     {
+        //A daily report for students of The Tech Academy.
+
         static void Main(string[] args)
         {
+            //Ask and save to a well-named variable and proper data type answers to the following questions
+
             Console.WriteLine("The Tech Academy");
             Console.WriteLine("Student Daily Report");
+
             Console.WriteLine("What course are you on?");
             string course_num = Console.ReadLine();
+
             Console.WriteLine("What page number?");
-            string page_num = Console.ReadLine();
+            int page_num = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Do you need help? Please answer \"true\" or \"false\"");
-            string help = Console.ReadLine();
+            bool help = Boolean.Parse(Console.ReadLine());
+
             Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
             string positive_exp = Console.ReadLine();
+
             Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
             string feedback = Console.ReadLine();
+
             Console.WriteLine("How many hours did you study today?");
-            string study_hours = Console.ReadLine();
+            float study_hours = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.ReadLine();
         }
     }
