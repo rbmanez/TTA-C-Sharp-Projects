2284a4e0735de4fd780906b3f9ad699015bf97d2
Robert <rbmanez@gmail.com>
2019-09-15 14:34:43 -0600

    create method exercise 3

---
+++ methodExercise3.cs/methodExercise3.cs/MathOperation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise3.cs
+{
+    //Create a class. In that class, create a method that will take in an integer,
+    //do a math operation to it and then return the answer as an integer.
+    class MathOperation
+    {
+        public int Add(int num)
+        {
+            num = num + 30;
+            return num;
+        }
+
+        //Add a second method to the class, with the same name, that will take in a decimal,
+        //do a different math operation to it and then return the answer as an integer.
+        public decimal Add(decimal num)
+        {
+            num = num + 25;
+            num = Convert.ToInt32(num);
+            return num;
+        }
+
+        //Add a third method to the class, with the same name, that will take in a string,
+        //convert it to an integer if possible, do a different math operation to it and then return the answer as an integer.
+        public int Add(string num)
+        {
+            int num2 = Convert.ToInt32(num);
+            num2 = num2 + 5;
+            return num2;
+        }
+    }
+}
---
+++ methodExercise3.cs/methodExercise3.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿//PURPOSE:    More method exercises
+//AUTHOR:     Robert Manez
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise3.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method of the console app, instantiate the class and call the one method,
+            //passing in an integer, such as 12. Then display the result to the screen.
+            MathOperation mathOp = new MathOperation();
+            Console.WriteLine(mathOp.Add(12));
+
+            //In the Main() method of the console app, instantiate the class and call the second method,
+            //passing in a decimal. Display the result to the screen.
+            Console.WriteLine(mathOp.Add(5.20m));
+
+            //In the Main() method of the console app, instantiate the class and call the third method,
+            //passing in a string that equates to an integer. Display the result to the screen.
+            Console.WriteLine(mathOp.Add("2"));
+
+            Console.ReadLine();
+        }
+    }
+}
