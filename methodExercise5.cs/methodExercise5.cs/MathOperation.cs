7bc5e9cabc18394dadd2629444914874c8f058cf
Robert <rbmanez@gmail.com>
2019-09-15 17:15:13 -0600

    create method exercise 5

---
+++ methodExercise5.cs/methodExercise5.cs/MathOperation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise5.cs
+{
+    //Create a class. In that class, create a void method that outputs an integer.
+    //Have the method divide the data passed to it by 2.
+    class MathOperation
+    {
+        public void DivideBy2(int num)
+        {
+            int num2 = num / 2;
+            Console.WriteLine(num + " divided by 2 equals " + num2);
+        }
+
+        //Create a method with output parameters.
+        public void Add(out int num)
+        {
+            num = 10;
+        }
+
+        //Overload a method.
+        public void Add(int num1, int num2, int num3)
+        {
+            int answer = num1 + num2 + num3;
+            Console.WriteLine(num1 + " plus " + num2 + " plus " + num3 + " equals " + answer);
+        }
+    }
+
+    //Declare a class to be static.
+    static class MathOperation2
+    {
+        public static int Multiply(int num)
+        {
+            num = num * 2;
+            return num;
+        }
+    }
+}
---
+++ methodExercise5.cs/methodExercise5.cs/Program.cs
@@ -0,0 +1,42 @@
+﻿//PURPOSE:    More method exercise
+//AUTHOR:     Robert Manez
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise5.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method, instantiate that class.
+            MathOperation math = new MathOperation();
+
+            //Have the user enter a number. Call the method on that number.
+            //Display the output to the screen. It should be the entered number, divided by two.
+            Console.WriteLine("Please enter a number.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            math.DivideBy2(num);
+
+            //Using method with out parameters.
+            Console.WriteLine("Out parameter example using method Add:");
+            int num2;
+            math.Add(out num2);
+            Console.WriteLine(num2);
+
+            //Using method overload
+            Console.WriteLine("Method overload example using method Add:");
+            math.Add(5, 10, 15);
+
+            //Using static class
+            Console.WriteLine("Static class example:");
+            Console.WriteLine(MathOperation2.Multiply(5));
+
+            Console.ReadLine();
+        }
+    }
+}
