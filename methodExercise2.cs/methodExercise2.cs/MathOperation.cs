010ea9973f25b2854e172b2cca0c34f8c667f77f
Robert <rbmanez@gmail.com>
2019-09-15 12:06:36 -0600

    create method exercise 2

---
+++ methodExercise2.cs/methodExercise2.cs/MathOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise2.cs
+{
+    //Create a class. In that class, create a void method that takes two integers as parameters.
+    //Have the method do a math operation on the first integer and display the second integer to the screen.
+    public class MathOperation
+    {
+        public void Add20(int num1, int num2)
+        {
+            num1 = num1 + 20;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ methodExercise2.cs/methodExercise2.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿//PURPOSE:    More method exercise
+//AUTHOR:     Robert Manez
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method of the console app, instantiate the class.
+            MathOperation mathOp = new MathOperation();
+            //Call the method in the class, passing in two numbers.
+            mathOp.Add20(10, 30);
+            //Call the method in the class, specifying the parameters by name.
+            mathOp.Add20(num1: 10, num2: 30);
+            Console.ReadLine();
+        }
+    }
+}
