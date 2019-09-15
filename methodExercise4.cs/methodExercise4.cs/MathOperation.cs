8e93fef24a94471125b295bba3822cfec68348c3
Robert <rbmanez@gmail.com>
2019-09-15 16:04:09 -0600

    create method exercise 4

---
+++ methodExercise4.cs/methodExercise4.cs/MathOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise4.cs
+{
+    //Create a class. In that class, create a method that takes two integers as parameters.
+    //Make one of them optional. Have the method do a math operation and return an integer result.
+    class MathOperation
+    {
+        public int Add(int num1, int num2 = 5)
+        {
+            num1 = num1 + num2 + 10;
+            return num1;
+        }
+    }
+}
---
+++ methodExercise4.cs/methodExercise4.cs/Program.cs
@@ -0,0 +1,40 @@
+﻿//PURPOSE:    More method exercise
+//AUTHOR:     Robert Manez
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise4.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method of the console app, instantiate the class.
+            MathOperation mathOp = new MathOperation();
+
+            //Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
+            Console.WriteLine("Please enter two numbers, one at a time. The second number is optional.");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            string num2 = Console.ReadLine();
+
+            //Call the method in the class, passing in the one or two numbers entered.
+            //Try various combinations of numbers on the code, including having no second number.
+            int number2;
+            if (!String.IsNullOrEmpty(num2))
+            {
+                number2 = Convert.ToInt32(num2);
+                Console.WriteLine(mathOp.Add(number1, number2));
+            }
+            else
+            {
+                Console.WriteLine(mathOp.Add(number1));
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
