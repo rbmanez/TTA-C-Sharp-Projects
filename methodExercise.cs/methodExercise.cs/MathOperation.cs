abff9cafbc98c6832441f0d8baf4c5b7880e4dba
Robert <rbmanez@gmail.com>
2019-09-15 10:34:10 -0600

    create method exercise

---
+++ methodExercise.cs/methodExercise.cs/MathOperation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise.cs
+{
+    //Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer.
+    //The methods should do some math operation on the received parameter. Put this class in a separate .cs file in the application.
+    public class MathOperation
+    {
+        public static int Add10(int num)
+        {
+            num = num + 10;
+            return num;
+        }
+        public static int Multiply10(int num)
+        {
+            num = num * 20;
+            return num;
+        }
+        public static int Square(int num)
+        {
+            num = num * num;
+            return num;
+        }
+    }
+}
---
+++ methodExercise.cs/methodExercise.cs/Program.cs
@@ -0,0 +1,27 @@
+﻿//PURPOSE:    Method exercise
+//AUTHOR:     Robert Manez
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask the user what number they want to do the math operations on
+            Console.WriteLine("Input a number to perform math operations on.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            //Call each method in turn, passing the user input to the method. Display the returned integer to the screen.
+            Console.WriteLine(userInput + " plus 10 equals " + MathOperation.Add10(userInput));
+            Console.WriteLine(userInput + " times 10 equals " + MathOperation.Multiply10(userInput));
+            Console.WriteLine(userInput + " squared equals " +  MathOperation.Square(userInput));
+            Console.ReadLine();
+        }
+    }
+}
