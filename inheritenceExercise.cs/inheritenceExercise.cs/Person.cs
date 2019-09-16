d88be0545f6c34e01927f915f19023157287f83a
Robert <rbmanez@gmail.com>
2019-09-16 11:56:48 -0600

    create inheritence exercise

---
+++ inheritenceExercise.cs/inheritenceExercise.cs/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritenceExercise.cs
+{
+    //Create a class called Person and give it two properties, each of data type string.
+    //One called FirstName, the other LastName.
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Give this class a void method called SayName() that takes no parameters and
+        //simply writes the person's full name to the console in the format of: "Name: [full name]".
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ inheritenceExercise.cs/inheritenceExercise.cs/Program.cs
@@ -0,0 +1,27 @@
+﻿//PURPOSE:    Inheritence exercise
+//AUTHOR:     Robert Manez
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritenceExercise.cs
+{
+    class Program
+    {
+        //Inside of the Main method, instantiate and initialize an Employee object
+        //with a first name of "Sample" and a last name of "Student".
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            //Call the superclass method SayName() on the Employee object.
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
