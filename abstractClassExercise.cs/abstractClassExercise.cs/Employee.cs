79e1e1e27e00a3b851ac23ae4d6acb5b878d8af8
Robert <rbmanez@gmail.com>
2019-09-16 15:03:08 -0600

    create abstract class exercise

---
+++ abstractClassExercise.cs/abstractClassExercise.cs/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractClassExercise.cs
+{
+    //Create another class called Employee and have it inherit from the Person class.
+    public class Employee : Person
+    {
+        //Implement the SayName() method inside of the Employee class.
+        public override void SayName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ abstractClassExercise.cs/abstractClassExercise.cs/Program.cs
@@ -0,0 +1,24 @@
+﻿//PURPOSE:    Abstract class exercise
+//AUTHOR:     Robert Manez
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractClassExercise.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inside the Main() method, instantiate an Employee object with firstName "Sample" and
+            //lastName "Student". Call the SayName() method on the object.
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+        }
+    }
+}
