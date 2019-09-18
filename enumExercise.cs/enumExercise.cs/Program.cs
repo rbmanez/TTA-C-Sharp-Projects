dfb72cfa791081e4b4ac9f8db7ce52acc1e1b565
Robert <rbmanez@gmail.com>
2019-09-18 16:29:28 -0600

    create enum exercise

---
+++ enumExercise.cs/enumExercise.cs/Program.cs
@@ -0,0 +1,45 @@
+﻿//PURPOSE:    Enum exercise
+//AUTHOR:     Robert Manez
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumExercise.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Wrap in a try/catch block and have it print "Please enter an actual day of the week." to the console if an error occurs.
+            try
+            {
+                //Prompt the user to enter the current day of the week.
+                Console.WriteLine("Please enter the current day of the week.");
+                //Assign the value to a variable of that enum data type you just created.
+                DaysOfTheWeek day;
+                Enum.TryParse(Console.ReadLine(), out day);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.ReadLine();
+        }
+
+        //Create an enum for the days of the week.
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thurday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
