275736311a095181438c2d518cd060da7eb08bbb
Robert <rbmanez@gmail.com>
2019-09-22 17:15:21 -0600

    create constant, variable, and constructor chaining exercise

---
+++ constVarConstructorChainEx.cs/constVarConstructorChainEx.cs/Program.cs
@@ -0,0 +1,23 @@
+﻿//PURPOSE:    Const, variable, constructor chaining exercise
+//AUTHOR:     Robert Manez
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constVarConstructorChainEx.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a const variable
+            const string name = "Robert";
+
+            //Create a variable using the keyword "var".
+            var number = 20;
+        }
+    }
+}
---
+++ constVarConstructorChainEx.cs/constVarConstructorChainEx.cs/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constVarConstructorChainEx.cs
+{
+    public class User
+    {
+        //Chain two constructors together.
+        public User() : this(31)
+        {
+
+        }
+        public User(int age)
+        {
+            age = age;
+        }
+    }
+}
