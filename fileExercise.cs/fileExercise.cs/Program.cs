69d5ef8d27b8ed4ff599101fc2d558b79b7fc127
Robert <rbmanez@gmail.com>
2019-09-22 13:37:25 -0600

    create file i/o exercise

---
+++ fileExercise.cs/fileExercise.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿//PURPOSE:    File I/O exercise
+//AUTHOR:     Robert Manez
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace fileExercise.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask a user for a number.
+            Console.WriteLine("Enter a number.");
+            string number = Console.ReadLine();
+
+            //Log that number to a text file.
+            File.WriteAllText(@"C:\Users\Robert\Documents\TTA\log.txt", number);
+
+            //Print the text file back to the user.
+            string readText = File.ReadAllText(@"C:\Users\Robert\Documents\TTA\log.txt");
+            Console.WriteLine(readText);
+            Console.ReadLine();
+        }
+    }
+}
