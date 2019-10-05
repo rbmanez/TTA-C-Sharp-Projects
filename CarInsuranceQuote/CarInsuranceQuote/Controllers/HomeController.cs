ed5968c92e4ae2933c6db75d82e7a01d5c2b4a10
Robert <rbmanez@gmail.com>
2019-10-05 15:07:02 -0600

    create car insurance quote assignment

---
+++ CarInsuranceQuote/CarInsuranceQuote/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿//PURPOSE:    Build an application for generating quotes for car insurance
+//AUTHOR:     Robert Manez
+
+using CarInsuranceQuote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsuranceQuote.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //passing in user input values
+        public ActionResult Estimate(string firstName, string lastName, string email, DateTime birthday, int carYear,
+                                     string carMake, string carModel, string dui, int speedingTickets, string coverage)
+        {
+            //accessing database
+            using (CarInsuranceEntities1 db = new CarInsuranceEntities1())
+            {
+                //instantiating a class object and assigning it properties based on user input
+                var customer = new Customer();
+                customer.FirstName = firstName;
+                customer.LastName = lastName;
+                customer.Email = email;
+                customer.Birthday = birthday;
+                customer.CarYear = carYear;
+                customer.CarMake = carMake;
+                customer.CarModel = carModel;
+                customer.DUI = dui;
+                customer.SpeedingTickets = speedingTickets;
+                customer.Coverage = coverage;
+
+                //adding and saving new class object to database
+                db.Customers.Add(customer);
+                db.SaveChanges();
+            }
+
+            return View("Estimate");
+        }
+    }
+}
